0ad447cb4f7b87d511114beb7108424c0a3fad0e
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-11 14:08:30 -0400

    Delaying search input change

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const fetchData = async (searchTerm) => {
     console.log(response.data);
 };
 
-const input = document.querySelector('input')
-input.addEventListener('input', (event) => {
-    fetchData(event.target.value)
-});
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = event => {
+    if(timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value)
+    }, 500)
+};
+
+input.addEventListener('input', onInput );
